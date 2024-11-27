b470303fc695229f777494dd33cda8fd9ff433e5
DanielRuiz <danimail952@gmail.com>
2024-11-28 00:53:08 +0100

    initial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { AccessComponent } from "./access/access.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AccessComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
